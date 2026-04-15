0b160cdbe9ad3098aa6f98fd13a455c159b94866
lysa omondi <lisabethkidangasi@gmail.com>
2026-04-15 05:10:41 +0300

    test

--- index.js
+++ index.js
@@ -4,3 +4,16 @@
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+function combineUsers(...args) {
+  const combineObject = {};{
+  combineObject.users = [];
+};
+
+for (let i = 0; i < args.length; i++) {
+  const arr = args[i];
+ combineObject.users = [...combineObject.users, ...arr];
+}
+
+combineObject.merge_date =new Date().toString("M/d/yyyy");
+return combineObject;
+};
